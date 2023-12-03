c3e41f15b33fcba2516f8fed7552967ba1be83d1
brcshakil1 <brcshakil72@gmail.com>
2023-12-03 12:05:26 +0600

    client site data fetcing

---
+++ src/app/users/page.js
@@ -0,0 +1,12 @@
+import Users from "@/components/Users/Users";
+
+const page = () => {
+  return (
+    <div className="p-5">
+      <h2 className="text-xl font-semibold">All Users</h2>
+      <Users />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h3 className="text-xl">Total user: {users.length}</h3>
+      <div>
+        {users?.map((user) => (
+          <div
+            key={user.id}
+            className="card w-[70vw] mx-auto bg-primary text-primary-content my-5"
+          >
+            <div className="card-body">
+              <h2 className="card-title">{user?.id}</h2>
+              <p>{user?.name}</p>
+              <p>{user?.email}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
